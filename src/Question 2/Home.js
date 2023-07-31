e0dc9d52857a31139d302479a6131143f9d3aea3
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-07-31 11:47:17 +0530

    question2 api single gET

--- src/Question 2/Home.js
+++ src/Question 2/Home.js
@@ -1,34 +1,65 @@
 import React, { useState } from "react";
 import "./Home.css";
+import  './alltrains'
 function Home() {
-    let [trains,dataFun]=useState({
-        train:[]
-    })
+  let [trains, dataFun] = useState({
+    train: [],
+  });
   function allTrains() {
     fetch("http://20.244.56.144:80/train/trains", {
       method: "GET",
       headers: {
-        "access_token":
-          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA3ODIwODgsImNvbXBhbnlOYW1lIjoiVHJhaW5DZW50cmFsUmFnaGF2IiwiY2xpZW50SUQiOiJiMDM3MzNlMi0xNTY2LTRlNGMtOGQ0My03N2U2ODYwOTc4MjMiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiMjEwMDY0MDEwOTAwNyJ9.ceMedyJJ14lJ-R_zZw8xuKeGfeDHtLFzppfn-XBJHt8",
+        access_token:
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA3ODQwMzQsImNvbXBhbnlOYW1lIjoiVHJhaW5DZW50cmFsUmFnaGF2IiwiY2xpZW50SUQiOiJiMDM3MzNlMi0xNTY2LTRlNGMtOGQ0My03N2U2ODYwOTc4MjMiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiMjEwMDY0MDEwOTAwNyJ9.oTODiP6vek6ZaL6-er8JkYggTgKWcbwoH-8CuMRmQME",
       },
     })
       .then((response) => response.json())
       .then((response) => {
         dataFun({
-            train:response
-        })
+          train: response,
+        });
         console.log(response);
       })
       .catch((err) => {
         console.log(err);
       });
   }
-  return (<div>
-    <div className="container">
+  return (
+    <div>
+      <div className="container">
+        <table class="table">
+          <thead>
+            <tr>
+              <th scope="col">#</th>
+              <th scope="col">First</th>
+              <th scope="col">Last</th>
+              <th scope="col">Handle</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th scope="row">1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th scope="row">2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th scope="row">3</th>
+              <td colspan="2">Larry the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </table>
         <button onClick={allTrains}>Click</button>
-
+      </div>
     </div>
-  </div>);
+  );
 }
 
 export default Home;
