be9af233746ef798376ceb162be09468705d8a64
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-07-31 11:03:22 +0530

    Question2 api done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Question 2/Home';
 
 function App() {
   return (
     <div className="App">
-      
+      <Home />
     </div>
   );
 }
