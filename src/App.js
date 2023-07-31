75eaf1f8f0e30d86216ddc3a13f26fe72d53ecc0
RAGHAV GARG <raghavgarg.hcst.cs20@sgei.org>
2023-07-31 11:12:25 +0530

    Question2 api done

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 import Home from './Question 2/Home';
 
 function App() {
--- src/Question 2/Home.js
+++ src/Question 2/Home.js
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
+import "./Home.css";
+function Home() {
+    let [trains,dataFun]=useState({
+        train:[]
+    })
+  function allTrains() {
+    fetch("http://20.244.56.144:80/train/trains", {
+      method: "GET",
+      headers: {
+        "access_token":
+          "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJleHAiOjE2OTA3ODIwODgsImNvbXBhbnlOYW1lIjoiVHJhaW5DZW50cmFsUmFnaGF2IiwiY2xpZW50SUQiOiJiMDM3MzNlMi0xNTY2LTRlNGMtOGQ0My03N2U2ODYwOTc4MjMiLCJvd25lck5hbWUiOiIiLCJvd25lckVtYWlsIjoiIiwicm9sbE5vIjoiMjEwMDY0MDEwOTAwNyJ9.ceMedyJJ14lJ-R_zZw8xuKeGfeDHtLFzppfn-XBJHt8",
+      },
+    })
+      .then((response) => response.json())
+      .then((response) => {
+        dataFun({
+            train:response
+        })
+        console.log(response);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }
+  return (<div>
+    <div className="container">
+        <button onClick={allTrains}>Click</button>
 
-function Home(){
-
+    </div>
+  </div>);
 }
 
 export default Home;
